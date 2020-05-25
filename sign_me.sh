c4493f32c0bbd1127354957f2de76b38a5657a63
Luke Spademan <info@lukespademan.com>
2020-05-25 20:52:27 +0100

    added index file

---
+++ sign_me.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+function usage {
+	cat << EOF
+Usage: $0 [OPTIONS]... [FILE]...
+
+Sign and date a text file.
+
+With no FILE, or when FILE is -, read standard input.
+
+When no FILE is speicifed and only an SSID is provided the system will search your saved Wi-Fi networks for encryption type and psk.
+If no match is found it will be assumed that there is no encryption on the Wi-Fi network.
+
+  -o --output   file to store output
+  -d --date     custom date to append to file
+  -h --help     show this help
+EOF
+}
+
+echo_error() {
+	cat <<< "$@" 1>&2;
+}
+
+function exit_error {
+	echo_error $1
+	usage
+	exit 1
+}
+
+
+# =Argument Parsing=
+# based on SO answer https://stackoverflow.com/a/14203146
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+	-o|--output)
+		OUTPUT_FILE="$2"
+		shift # past argument
+		shift # past value
+		;;
+	-d|--date)
+		DATE="$2"
+		shift # past argument
+		shift # past value
+		;;
+	-h|--help)
+		exit_error
+		;;
+	*)    # unknown option
+		POSITIONAL+=("$1") # save it in an array for later
+		shift # past argument
+		;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+INPUT_FILE=${POSITIONAL[0]}
+
+# =End Argument Parsing=
+
+# Check that an input file is supplied
+if [ "$INPUT_FILE"  == "-" ]; then
+    INPUT_FILE = ""
+
+fi
+
+if [ -z $OUTPUT_FILE ]; then
+    OUTPUT_FILE="${INPUT_FILE:-$(date '+%Y-%m-%d-%s%N')}.asc"
+fi
+
+TMP_FILE=$(mktemp)
+
+# Create the tmp file from either stdin or the given file
+while read line
+do
+  echo "$line" >> $TMP_FILE
+done < "${INPUT_FILE:-/dev/stdin}"
+echo -e "\n\nCorrect as of ${DATE:-$(date --utc)}" >> $TMP_FILE
+
+
+gpg --clear-sign --output ${OUTPUT_FILE:-$INPUT_FILE.asc} $TMP_FILE
+
+rm $TMP_FILE
+
